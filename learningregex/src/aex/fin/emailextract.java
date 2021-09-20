13eee7755e6421fde4f17d730bcc8060ae544d2b
Geofed <theglassofdawn@gmail.com>
2021-09-20 11:48:53 +0100

    added showing the top 20 domains

--- learningregex/src/aex/fin/emailextract.java
+++ learningregex/src/aex/fin/emailextract.java
@@ -3,9 +3,11 @@ package aex.fin;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.regex.*;
+import java.util.Map;
 import java.util.HashMap;
-
 public class emailextract {
 
 
@@ -14,13 +16,37 @@ public class emailextract {
         Pattern pattern = Pattern.compile("[a-z-._]+@([a-z0-9_.-]+[a-z])", Pattern.CASE_INSENSITIVE);
         String emailTextFile = "/Users/ftarthur/Desktop/Training/emailextraction/learningregex/src/sample.txt";
 
-        System.out.println(FindFrequencyOfEmails(pattern, emailTextFile));
+        Map<String, Integer> emails = FindFrequencyOfEmails(pattern, emailTextFile);
+
+        PrintMostUsedDomain(emails);
 
     }
 
-    private static HashMap<String, Integer> FindFrequencyOfEmails(Pattern pattern, String input) {
+    private static void PrintMostUsedDomain(Map<String, Integer> emails) {
 
-        HashMap<String, Integer> emails = new HashMap<String, Integer>();
+        Map<String, Integer> sortedEmails = new LinkedHashMap<>();
+        emails.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> sortedEmails.put(x.getKey(), x.getValue()));
+
+        System.out.println(sortedEmails);
+
+
+        Integer count  = 0;
+        for (String key : sortedEmails.keySet()) {
+            count++;
+            if (count <= 10) {
+                System.out.println(key + " domain appears " + sortedEmails.get(key) + " time(s)");
+            }
+        }
+
+    }
+
+    private static Map<String, Integer> FindFrequencyOfEmails(Pattern pattern, String input) {
+
+        Map<String, Integer> emails = new HashMap<>();
+
         try {
             FileReader reader = new FileReader(input);
             BufferedReader buff = new BufferedReader(reader);
